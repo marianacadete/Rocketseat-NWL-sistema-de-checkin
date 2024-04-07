3b87eaeb53c308802157ee7f6be70dc951a3a840
Mariana Cadete <marianations@gmail.com>
2024-04-07 18:25:50 -0300

    Conclusão aula 2

---
+++ src/routes/get-event.ts
@@ -0,0 +1,45 @@
+import {FastifyInstance} from "fastify";
+import {ZodTypeProvider} from "fastify-type-provider-zod";
+import {z} from "zod";
+import { prisma } from "../lib/prisma";
+
+
+export async function getEvent(app: FastifyInstance) {
+    app
+    .withTypeProvider<ZodTypeProvider>()
+    .get('/events/:eventId', {
+        schema: {
+            params: z.object({
+                eventId: z.string().uuid(),
+            }),
+            response: {},
+        }
+    } , async (request,reply) => {
+        const {eventId} = request.params
+
+        const event = await prisma.event.findUnique ({
+            select: {
+                id: true,
+                title: true,
+                slug: true,
+                details: true,
+                maximumAttendees: true,
+                _count: {
+                    select:{
+                        attendees:true,
+                    }
+                },
+            },
+            where: {
+                id: eventId,
+            }
+        })
+
+        if (event === null) {
+            throw new Error('Event not found.')
+        }
+
+        return reply.send({event})
+
+    })
+}
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import fastify from "fastify";
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
+import { getEvent } from "./routes/get-event";
+import { getAttendeeBadge } from "./routes/get-attendee-bagde";
 
 const app = fastify()
 
@@ -10,6 +12,8 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent)
 app.register(registerForEvent)
+app.register(getEvent)
+app.register(getAttendeeBadge)
 
 app.listen({ port:3333}).then(() => {
     console.log('HTTP server running!')
