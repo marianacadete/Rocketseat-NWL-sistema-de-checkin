ba3c7b5af8789ec609ed20f0ac7fb75bc6cde3f5
Mariana Cadete <marianations@gmail.com>
2024-04-07 21:38:16 -0300

    Conclusão aula 32

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,34 @@
 import fastify from "fastify";
-import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+import { jsonSchemaTransform, serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
 import { getAttendeeBadge } from "./routes/get-attendee-bagde";
+import { checkIn } from "./routes/check-in";
+import { getEventAttendees } from "./routes/get-event-attendees";
+
 
 const app = fastify()
+
+app.register(fastifySwagger, {
+    swagger: {
+        consumes: ['application/json'],
+        produces: ['application/json'],
+        info: {
+            title: 'pass.in',
+            description: 'Especificações da API para o back-end da aplicação pass.in contruida durante o NLW da Rocketseat',
+            version: '1.0.0',
+
+        },
+    },
+    transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+    routePrefix: '/docs',
+})
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
@@ -14,6 +37,8 @@ app.register(createEvent)
 app.register(registerForEvent)
 app.register(getEvent)
 app.register(getAttendeeBadge)
+app.register(checkIn)
+app.register(getEventAttendees)
 
 app.listen({ port:3333}).then(() => {
     console.log('HTTP server running!')
